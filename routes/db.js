fcb0cdb6a4445098c9622d3aab7261227730e232
Devin Wulf <devinlwulf@gmail.com>
2022-04-02 16:14:04 -0500

    updated server.js

--- routes/db.js
+++ routes/db.js
@@ -1,69 +1,39 @@
 const db = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-const { v4: uuidv4 } = require('uuid');
 
-// // GET Route for retrieving the database
-// db.get('/api/db', (req, res) => {
-//   console.info(`${req.method} request received for database`);
 
-//   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-// });
-
-// // POST Route for submitting database
-// db.post('/api/db', (req, res) => {
-//   // Log that a POST request was received
-//   console.info(`${req.method} request received to submit to database`);
-
-//   // Destructuring assignment for the items in req.body
-//   const { title, text } = req.body;
-
-//   // If all the required properties are present
-//   if (title && text) {
-//     // Variable for the object we will save
-//     const newDb = {
-//       title,
-//       text,
-//       notes_id: uuid(),
-//     };
-
-//     readAndAppend(newDb, './db/db.json');
-
-//     const response = {
-//       status: 'success',
-//       body: newDb,
-//     };
-
-//     res.json(response);
-//   } else {
-//     res.json('Error in posting feedback');
-//   }
-// });
-
-// GET Route for retrieving all db
-db.get('/api/notes', (req, res) => {
-    readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
-  });
-
-  // POST Route for a new note
+// POST request method to add note;
 db.post('/api/notes', (req, res) => {
-    console.log(req.body);
-  
-    const { title, text } = req.body;
-  
-    if (req.body) {
-      const newNote = {
-        title,
-        text,
-        note_id: uuidv4(),
-      };
-  
-      readAndAppend(newNote, './db/db.json');
-      res.json(`Note added successfully 🚀`);
-    } else {
-      res.error('Error in adding Note');
-    }
-  });
-  
+	let response;
+	const { title, text } = req.body;
+	if (title && text) {
+		const newNote = {title, text};
+		response = {
+			status: 'Success',
+			body: newNote
+		};
+		readAndAppend(newNote, './db/db.json');
+		console.log(response);
+		res.status(201).json(response);
+	} else {
+		res.status(500).json('Error in posting note')
+	}
+})
 
-  
-  module.exports = db;
+db.post('/notes', (req, res) => {
+	let response;
+	const { title, text } = req.body;
+	if (title && text) {
+		const newNote = {title, text};
+		response = {
+			status: 'Success',
+			body: newNote
+		};
+		readAndAppend(newNote, './db/db.json');
+		console.log(response);
+		res.status(201).json(response);
+	} else {
+		res.status(500).json('Error in posting note')
+	}
+})
+
+module.exports = db;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const { clog } = require("./middleware/clog");
 const api = require("./routes/index");
 const db = require("./db/db.json");
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,35 +17,49 @@ app.use("/api", api);
 app.get("/", (req, res) => res.sendFile("Navigate to /notes or /routes"));
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
 
+
+// GET request method;
 app.get("/api/notes", (req, res) => {
-	res.json(db)
-	const { title, text } = req.body;
-	if (title && text) {
-		const newNote = {title, text};
-		const response = {
-			status: 'Success',
-			body: newNote
-		};
-
-		console.log(response);
-		res.status(201).json(response);
-	} else {
-		res.status(500).json('Error in posting note')
-	}
+	console.log(db)
+	return res.json(db);
 });
 
-app.get("/api/notes/:title", (req, res) => {
-	console.log(req.params);
-	const requestedTitles = req.params.title.toLowerCase();
-	const foundTitle = db.filter(
-		(termObj) => termObj.term.toLowerCase() === requestedTitles
-	)[0];
-	console.log(foundTitle);
-	if (requestedTerm === db[i].title.toLowerCase()) {return res.json(db[i])}
-});
+// // POST request method to add note;
+// app.post('/api/notes', (req, res) => {
+// 	let response;
+// 	const { title, text } = req.body;
+// 	if (title && text) {
+// 		const newNote = {title, text};
+// 		response = {
+// 			status: 'Success',
+// 			body: newNote
+// 		};
+// 		readAndAppend(newNote, './db/db.json');
+// 		console.log(response);
+// 		res.status(201).json(response);
+// 	} else {
+// 		res.status(500).json('Error in posting note')
+// 	}
+// })
+
+// app.post('/notes', (req, res) => {
+// 	let response;
+// 	const { title, text } = req.body;
+// 	if (title && text) {
+// 		const newNote = {title, text};
+// 		response = {
+// 			status: 'Success',
+// 			body: newNote
+// 		};
+// 		readAndAppend(newNote, './db/db.json');
+// 		console.log(response);
+// 		res.status(201).json(response);
+// 	} else {
+// 		res.status(500).json('Error in posting note')
+// 	}
+// })
 
 app.get("*", (req, res) => res.send("File Not Found"));
-
 app.listen(PORT, () =>
 	console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
